a6ef21adb7a75faabdb05584aaa2f7b6d05b08fe
Ebarim Fortune <ebarimjesus1@gmail.com>
2023-04-21 03:40:01 +0000

    update

--- wizapp/talkgpt/views.py
+++ wizapp/talkgpt/views.py
@@ -22,7 +22,7 @@ from django.http import HttpResponse
 
 
 
-openai.api_key = "sk-RDITFbLRYJrcw0H9vPIQA3iuYP6WB1XJ9"
+openai.api_key = "sk-RDITFbLRYJ3iuYP6WB1XJ9"
 
 
 
--- wizapp/wizapp/urls.py
+++ wizapp/wizapp/urls.py
@@ -1,6 +1,4 @@
 from django.views.generic.base import RedirectView
-
-
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
@@ -11,33 +9,19 @@ from django.urls import include, path
 from talkgpt.views import send_message, stream_response
 from talkgpt.views import LandingPageView
 from talkgpt.views import ExamplesPageView, talkgpt
-
 admin.autodiscover()
-
-urlpatterns = [
-    path("sitemap.xml", sitemap, {"sitemaps": {"cmspages": CMSSitemap}}),
-    path('admin/', admin.site.urls),
-    path('en/talkgpt/', include('talkgpt.urls')),
-
-]
-
-urlpatterns += i18n_patterns(path("", include("cms.urls")))
-
-# This is only needed when using runserver.
+urlpatterns = [path('sitemap.xml', sitemap, {'sitemaps': {'cmspages':
+    CMSSitemap}}), path('admin/', admin.site.urls), path('en/talkgpt/',
+    include('talkgpt.urls')), path('', include('djangocms_blog.taggit_urls'))]
+urlpatterns += i18n_patterns(path('', include('cms.urls')))
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
-    path("send_message/", send_message, name="send_message"),
-    path("stream_response/", stream_response, name="stream_response"),
-    path('send_message', RedirectView.as_view(url='/send_message/', permanent=True)),
-    path('', LandingPageView.as_view(), name='landing'),
-    path('examples/', ExamplesPageView.as_view(), name='examples'),
-    path('talkgpt/', talkgpt, name='talkgpt'),
-]
-
-
-urlpatterns += [
-    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        )
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.
+        STATIC_ROOT)
+urlpatterns += [path('send_message/', send_message, name='send_message'),
+    path('stream_response/', stream_response, name='stream_response'), path
+    ('send_message', RedirectView.as_view(url='/send_message/', permanent=
+    True)), path('', LandingPageView.as_view(), name='landing'), path(
+    'examples/', ExamplesPageView.as_view(), name='examples'), path(
+    'talkgpt/', talkgpt, name='talkgpt')]
